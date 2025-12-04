6e04a68ccec603f42774421a59e7d955e4f41aee
dreamAviator <nathan@baron.berlin>
2025-12-04 22:49:09 +0100

    folder select

--- lib/home/library_screen/library_tab_bar/folders_tab/folders_tab.dart
+++ lib/home/library_screen/library_tab_bar/folders_tab/folders_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flatter/home/library_screen/library_tab_bar/folders_tab/folders_tab_ViewModel.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class FoldersTab extends StatelessWidget {
   const FoldersTab({super.key,required this.viewModel});
@@ -7,8 +8,31 @@ class FoldersTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("this is folders tab"),
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: viewModel.addFolder, icon: Icon(Icons.folder))
+        ],
+        title: ListenableBuilder(
+          listenable: viewModel,
+          builder: (context,_) {
+            return Text(viewModel.title);
+          },
+        )
+      ),
+      body: ListenableBuilder(
+        listenable: viewModel,
+        builder: (context,_) {
+          return ListView.builder(
+            itemCount: viewModel.directories.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: Text(viewModel.directories[index]),
+              );
+            },
+          );
+        },
+      )
     );
   }
 }
--- lib/home/library_screen/library_tab_bar/folders_tab/folders_tab_ViewModel.dart
+++ lib/home/library_screen/library_tab_bar/folders_tab/folders_tab_ViewModel.dart
@@ -1,5 +1,16 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 
 class FoldersTabViewModel extends ChangeNotifier {
-
+  String title = "Folders";
+  String directoryString = "no directory selected";
+  List<String> directories = [];//mwegen berechtigungen soll man einfach mehrere ordner hinzufügen, auf alle unterordner hat die app dann zugriff; man hat also eine liste mit den hinzugefügten ordnern.
+  Future<void> addFolder() async {
+    title = "yoo it changed";
+    String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+    if (selectedDirectory != null) {
+      directories.add(selectedDirectory);
+    }
+    notifyListeners();
+  }
 }
