691db15407c08c93c03e5a62969fcacb842bacc4
dreamaviator <130237036+dreamAviator@users.noreply.github.com>
2026-05-21 23:08:22 +0200

    continued full rework of player finished subsonicSong to MediaItem

--- lib/useful_scripts.dart
+++ lib/useful_scripts.dart
@@ -2,7 +2,29 @@ import 'package:audio_service/audio_service.dart';
 
 class SubsonicJustAudioCompatibility {
 
-  List<MediaItem> subsonicSongListToMediaItemList(List<Map<dynamic,dynamic>>) {
+  List<MediaItem> subsonicSongListToMediaItemList(List<Map<dynamic,dynamic>> songList) {
+    List<MediaItem> mediaItemList = [];
+    for (Map<dynamic,dynamic> song in songList) {
+      mediaItemList.add(subsonicSongToMediaItem(song));
+    }
+    return mediaItemList;
+  }
 
+  MediaItem subsonicSongToMediaItem(Map<dynamic,dynamic> song) {
+    String id = song['id'];
+    String title = song['title'];
+    String album = song['album'];
+    String artist = song['artist'];
+    Duration duration = Duration(seconds: song['duration']);
+    Map<String,dynamic> extras = {};
+    song.forEach((key,value) {
+      extras[key] = value;
+    });
+    song.remove('id');
+    song.remove('title');
+    song.remove('album');
+    song.remove('artist');
+    song.remove('duration');
+    return MediaItem(id: id, title: title, album: album, artist: artist, duration: duration,extras: extras);//rating noch rein
   }
 }
